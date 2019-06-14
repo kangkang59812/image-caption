d6efcf305e2185347bced945b74b22d8b2cab742
kangkang59812 <596286458@qq.com>
2019-06-14 15:56:23 +0800

    add save all

--- utils/utils.py
+++ utils/utils.py
@@ -221,7 +221,7 @@ def clip_gradient(optimizer, grad_clip):
                 param.grad.data.clamp_(-grad_clip, grad_clip)
 
 
-def save_checkpoint(data_name, epoch, epochs_since_improvement, miml, decoder,decoder_optimizer,
+def save_checkpoint(data_name, epoch, epochs_since_improvement, miml, decoder, decoder_optimizer,
                     bleu4, is_best):
     """
     Saves model checkpoint.
@@ -243,6 +243,36 @@ def save_checkpoint(data_name, epoch, epochs_since_improvement, miml, decoder,de
              'decoder': decoder.state_dict(),
              'decoder_optimizer': decoder_optimizer.state_dict()}
     filename = 'checkpoint_withmiml' + data_name + '.pth.tar'
+    torch.save(state, filename)
+    # If this checkpoint is the best so far, store a copy so it doesn't get overwritten by a worse checkpoint
+    if is_best:
+        torch.save(state, 'BEST_' + filename)
+
+
+def saveall_checkpoint(data_name, epoch, epochs_since_improvement, miml, encoder, decoder, encoder_optimizer, decoder_optimizer,
+                       bleu4, is_best):
+    """
+    Saves model checkpoint.
+
+    :param data_name: base name of processed dataset
+    :param epoch: epoch number
+    :param epochs_since_improvement: number of epochs since last improvement in BLEU-4 score
+    :param encoder: encoder model
+    :param decoder: decoder model
+    :param encoder_optimizer: optimizer to update encoder's weights, if fine-tuning
+    :param decoder_optimizer: optimizer to update decoder's weights
+    :param bleu4: validation BLEU-4 score for this epoch
+    :param is_best: is this checkpoint the best so far?
+    """
+    state = {'epoch': epoch,
+             'epochs_since_improvement': epochs_since_improvement,
+             'bleu-4': bleu4,
+             'miml': miml.state_dict(),
+             'encoder': encoder.state_dict(),
+             'decoder': decoder.state_dict(),
+             'encoder_optimizer': encoder_optimizer.state_dict(),
+             'decoder_optimizer': decoder_optimizer.state_dict()}
+    filename = 'checkpoint_all' + data_name + '.pth.tar'
     torch.save(state, filename)
     # If this checkpoint is the best so far, store a copy so it doesn't get overwritten by a worse checkpoint
     if is_best:
